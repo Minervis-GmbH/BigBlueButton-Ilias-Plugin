499f4ec08f0d2a348fbc91ba3ccde7ae9112614e
sebastian <sebastian@ubuntu>
2014-05-14 08:23:06 +0200

    Show if meetings is recorded

--- plugin.php
+++ plugin.php
@@ -4,12 +4,12 @@
 $id = "xbbb";
  
 // code version; must be changed for all code changes
-$version = "1.0.1";
+$version = "1.0.2";
  
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
 $ilias_min_version = "4.0.0";
-$ilias_max_version = "4.4.0";
+$ilias_max_version = "4.4.999";
  
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible = "Andrés Ederra, Sebastian Ziegler";
