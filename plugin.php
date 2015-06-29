da4e6d5bf34c6b19544cbd9ddb1337aac6e5fa6b
sebastianz1980 <sebastian@ziegler-ziegler.de>
2015-06-29 02:50:51 -0700

    Fehlerbehebungen

--- plugin.php
+++ plugin.php
@@ -4,12 +4,12 @@
 $id = "xbbb";
  
 // code version; must be changed for all code changes
-$version = "1.0.3";
+$version = "1.0.4";
  
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
 $ilias_min_version = "4.0.0";
-$ilias_max_version = "4.4.999";
+$ilias_max_version = "5.0.999";
  
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible = "Andrés Ederra, Sebastian Ziegler";
