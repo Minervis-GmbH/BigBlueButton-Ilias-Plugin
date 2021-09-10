243c84be914e25c4a59cc9c12161bf642a838905
Jephte Abijuru <57417057+jeph864@users.noreply.github.com>
2021-09-10 14:40:51 +0200

    update max. version

--- plugin.php
+++ plugin.php
@@ -9,7 +9,7 @@ $version = "1.0.15";
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
 $ilias_min_version = "4.0.0";
-$ilias_max_version = "6.99";
+$ilias_max_version = "7.99";
  
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible = "Minervis GmbH";
