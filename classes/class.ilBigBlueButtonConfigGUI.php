ec35b85e23e2c7a278ea22f8af9901a787d6ac2c
Hannes <31671206+xJuvi@users.noreply.github.com>
2023-03-07 14:12:01 +0100

    Fix reachability check (#40)
    * fix reachability check
    
    If the API isn't reachable, the function returned faled. So the "try" part to catch the exception i only affected if an system error occours.
    
    * add not reachable language string
    
    * add server not reachable language string
    
    * add server not reachable language string
    
    * add server not reachable language string
    
    * add server not reachable language string
    
    * add server not reachable language string
    
    * add server not reachable language string
    
    * add server not reachable language string

--- classes/class.ilBigBlueButtonConfigGUI.php
+++ classes/class.ilBigBlueButtonConfigGUI.php
@@ -72,7 +72,7 @@ class ilBigBlueButtonConfigGUI extends ilPluginConfigGUI
         if ($values["svrpublicurl"] != '' && $values["svrsalt"] != '') {
             $server_reachable=$this->isServerReachable($values["svrpublicurl"], $values["svrsalt"]);
             if (!$server_reachable) {
-                ilUtil::sendFailure("server not reachable", true);
+                ilUtil::sendFailure($pl->txt("sever_not_reachable"), true);
             }
         }
 
@@ -227,12 +227,11 @@ class ilBigBlueButtonConfigGUI extends ilPluginConfigGUI
         include_once("./Customizing/global/plugins/Services/Repository/RepositoryObject/BigBlueButton/classes/class.ilBigBlueButtonProtocol.php");
         $bbb_helper=new BBB($salt,$url);
         try{
-            $bbb_helper->getApiVersion();
+            $apiVersion = $bbb_helper->getApiVersion();
+	    return $apiVersion->success();
         }catch (Exception $e) {
             return false;
         }
-        return true;
-
     }
 
 
